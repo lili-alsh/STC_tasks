09b1ef69e83068cd39b90a9745da7d0115af0c54
lili-alsh <Lili-ALSH@yandex.ru>
2020-06-20 11:13:01 +0300

    Vending machine with 5 sorts of drinks. VM can interact with user (selection of drink, give change)

---
+++ src/Task_4/VendingMachine/Drinks.java
@@ -0,0 +1,25 @@
+package Task_4.VendingMachine;
+
+public class Drinks {
+    private String drink;
+    private int price;
+    private byte button;
+
+    public Drinks(String drink, int price, byte button) {
+        this.drink = drink;
+        this.price = price;
+        this.button=button;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public byte getButton() {
+        return button;
+    }
+}
---
+++ src/Task_4/VendingMachine/Main.java
@@ -0,0 +1,78 @@
+package Task_4.VendingMachine;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] names={"Чай", "Кофе", "Какао", "Coca-cola","Fanta"};
+        int [] prices = {100, 100, 150, 200, 150};
+        byte [] buttons={1, 2, 3, 4, 5};
+        Drinks [] drinks=new Drinks[names.length];
+        for (int i = 0; i <drinks.length ; i++) {
+            drinks[i]=new Drinks(names[i],prices[i],buttons[i]);
+        }
+        Arrays.sort(prices);
+        Arrays.sort(buttons);
+        byte button=0;
+        int money=0;
+        String str="";
+        boolean isEnd=true;
+        Scanner scan=new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите депозит");
+            try {
+               money=Integer.parseInt(scan.next());
+               if (money<prices[0]) {
+                   System.out.println("Недостаточно средств");
+               } else {
+                   break;
+               }
+            } catch (NumberFormatException e) {
+                System.out.println("Недопустимый ввод. Повторите.");
+            }
+        }
+        condition:
+        while (true) {
+            System.out.println("Выберите напиток (указать номер кнопки):");
+            System.out.println("Чай - Кнопка 1, Кофе - Кнопка 2, Какао - Кнопка 3, " +
+                    "Coca-cola - Кнопка 4, Fanta - Кнопка 5");
+            try {
+                button=scan.nextByte();
+                for (Drinks drink:drinks) {
+                    if (button==drink.getButton()) {
+                        System.out.printf("Получите %s", drink.getDrink());
+                        System.out.println();
+                        money-=drink.getPrice();
+                        if (money>=prices[0]){
+                            isEnd=true;
+                            while (isEnd) {
+                                System.out.println("Что-нибудь еще?");
+                                System.out.println("ДА НЕТ");
+                                str=scan.next();
+                                if (str.equals("НЕТ")) {
+                                    if (money>0){
+                                        System.out.printf("Получите сдачу %d",money);
+                                        System.out.println();
+                                    }
+                                    break condition;
+                                } else if (str.equals("ДА")){
+                                   isEnd=false;
+                                } else  {
+                                    System.out.println("Повторите ввод");
+                                }
+                            }
+                        } else {
+                            break condition;
+                        }
+                    } else if (button>buttons[buttons.length-1] || button<buttons[0]) {
+                        System.out.println("Такого напитка нет в списке");
+                        continue condition;
+                    }
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка ввода. Повторите.");
+            }
+        }
+    }
+}
